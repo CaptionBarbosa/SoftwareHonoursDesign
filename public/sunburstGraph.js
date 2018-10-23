55ce82c5b67bd48f6a50fb36bceef9e76c17b393
CaptionBarbosa <alessandrodsbarbosa@gmail.com>
2018-10-23 11:39:50 +0200

    Small change to dummy data for Sunburst graph

--- public/sunburstGraph.js
+++ public/sunburstGraph.js
@@ -1,10 +1,11 @@
-//Initially sourced from Martin Etmajer - "Zoomable Sunburst with Labels"
-//Source - http://bl.ocks.org/metmajer/5480307
+//Initially sourced from Greg Venech - "Sunburst with Zooming and Breadcrumbs"
+//Source - https://codepen.io/gvenech/pen/KppEaP
 //Access: 19th October 2018
 //Edited for usage by Alessandro Barbosa
 function sunburstToggle()
 {
-    //document.getElementById("content").innerHTML='<object type="text/html" data="/cdn/sunburstGraph.html" height="80%" width="100%" ></object>';
-    document.getElementById("SunburstGraphDiv").style.display = "block";
+    document.getElementById("contentSunburstGraph").innerHTML='<object type="text/html" data="/cdn/sunburstGraph.html" height="80%" width="100%" ></object>';
+    document.getElementById("contentLineGraph").style.display = "none";
+    document.getElementById('contentSunburstGraph').style.display = "block"
     graph = "sunburstGraph";
 }
